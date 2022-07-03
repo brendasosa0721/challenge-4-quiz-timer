9794a65e095aced2b80257b5456990ce9c0c97fc
Brenda  Sosa <brenda.sosa@gmail.com>
2022-07-03 12:54:35 -0400

    add quiz box and css to this session

--- script.js
+++ script.js
@@ -9,3 +9,53 @@
 // THEN the game is over
 // WHEN the game is over
 // THEN I can save my initials and score
+
+/////////// Questions for the questions with correct and wrong answer.
+
+// 1. Whar 2 types of languages have you learned about in Tech Arts? (Not including javaScript)
+//html and coding Wrong
+//css and html Correct!
+//English and Spanish Wrong
+//binary and c++ Wrong
+
+// 2. HTML language
+// is the coding used to create Websites Correct!
+//is the coding used to create font sizes and colors Wrong
+// is the coding used to create drawings Wrong
+// is never used in the outside world Wrong
+
+// 3. If you added an h1 tag, what would it look like?
+// The smallest heading in the html language Wrong
+// This tag does not exits Wrong
+// The largest heading in html language Correct!
+// A different colored heading Wrong
+
+
+// 4. CSS language..
+// is the language used to create Websites Wrong
+// is the language used to create font sizes and colors Correct!
+// is the coding used to creates pictures Wrong
+// is never used in the outside world Wrong
+
+// 5. What would the coding look like for <span> a heading written in red?
+// { h1 color: red;} Wrong
+// <h1> color; red Wrong
+// h1 { color: green; } Wrong 
+// h1 { color: red; } Correct!
+
+
+
+// on Click replaced the start button with questions
+var startButtonEl = document.querySelector (".start-btn")
+console.log("hide")
+var mainBodyEl = document.querySelector(".body")
+
+
+// var createH2El = document.createElement ("h2")
+// createH2El.className = ("title")
+// createH2El.textContent = "hi I am a question"
+// mainBodyEl.appendChild (createH2El)
+// startButtonEl.addEventListener("click" , createH2El)
+
+
+
