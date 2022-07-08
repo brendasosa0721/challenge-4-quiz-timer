10db9f0b5ea32a54cdcf99fdb707cd82405fdbcf
Brenda  Sosa <brenda.sosa@gmail.com>
2022-07-07 23:02:03 -0400

    added the readme

--- README.md
+++ README.md
@@ -5,11 +5,18 @@
 Quiz Timer
 
 # Purpose
-
+This challenge was about creating a quiz with timer from scratch to test our knowledge on HTML, CSS and JS. I tried my best to complete as much as I could from the criteria provided
 
 # Website- Deployed URL/ Screenshot
 
+
+
+# Programs used
+
+JavaSCript, HTML and CSS
 # Constribution
+
+Made by Brenda Sosa
 
 
 
--- script.js
+++ script.js
@@ -95,10 +95,7 @@ startButton.addEventListener("click", function () {
         question4.classList.remove("hidden");
       });
       //Hidden Question 4 after clicking the next btn
-      nextq4.addEventListener("click", function () {
-        question4.classList.add("hidden");
-        endquiz.classList.remove("hide");
-      });
+      
     });
   
   });
